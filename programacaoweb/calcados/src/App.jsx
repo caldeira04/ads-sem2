6725b0ab3cd4a270dac891ef3d18b7062c0f75e5
caldeira04 <joao.p.caldeira@icloud.com>
2024-08-20 11:13:20 -0300

    aula02 pt2

---
+++ programacaoweb/calcados/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Caldeira Calçados</h1>
+      Confira os detalhes do tênis: Air Jordan 1 Low TS OG SP - Cream Blue
+
+    </>
+  )
+}
+
+export default App
