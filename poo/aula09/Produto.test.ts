7d265b01d55ab578ab22ce0c7fbfe4597637977b
caldeira04 <joao.p.caldeira@icloud.com>
2024-10-10 11:13:41 -0300

    socorro

---
+++ poo/aula09/Produto.test.ts
@@ -0,0 +1,3 @@
+test("Adquirir nome e valor de produto") () => {
+  const produto = new Produto("Bola", 10.0);
+});
---
+++ poo/aula09/Produto.ts
@@ -0,0 +1,5 @@
+export class Produto {
+  private nome: string;
+  private preco: number;
+  private estoque: number;
+}
